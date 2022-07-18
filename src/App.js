9a3edea742aaeeab122d2705382e67cbd654d2eb
Shubham <shubhamkumarsingh65@gmail.com>
2022-07-18 21:19:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Sdata from "./Sdata";
+import Card  from "./Card";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="header_style">LIST OF TOP NETFLIX SERIES</h1>
+      {Sdata.map((val) => {
+        return (
+          <Card
+            imgsrc={val.imgsrc}
+            title={val.title}
+            sname={val.sname}
+            link={val.link}
+          />
+        );
+      })}
+      ;
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/Sdata.js
@@ -0,0 +1,46 @@
+const Sdata = [
+  {
+    imgsrc:
+      "https://feed.muzlicdn.xyz/muzli_feed/wp-content/uploads/2020/10/11111515/global.png",
+    title: "A Netflix Original Series",
+    sname: "DARK",
+    link: "https://www.netflix.com/in/title/80100172",
+  },
+  {
+    imgsrc:
+      "https://images.ctfassets.net/4cd45et68cgf/5DIYYKy9JB7m90nozSIcM1/2acf7e826bbe6a656fd91c9179f17e0b/Stranger_Things__The_Experience.jpg?w=2560",
+    title: "A Netflix Original Series",
+    sname: "STRANGER THINGS",
+    link: "https://www.netflix.com/in/title/80057281",
+  },
+  {
+    imgsrc:
+      "https://www.liveakhbar.in/wp-content/uploads/2022/02/p185846_b_h9_ad.jpg",
+    title: "A Netflix Original Series",
+    sname: "BREAKING BAD",
+    link: "https://www.netflix.com/in/title/70143836",
+  },
+  {
+    imgsrc:
+      "https://netflixjunkie.com/wp-content/uploads/2021/12/Money-Heist.jpeg",
+    title: "A Netflix Original Series",
+    sname: "MONEY HEIST",
+    link: "https://www.netflix.com/in/title/80192098",
+  },
+  {
+    imgsrc:
+      "https://www.nextflicks.tv/wp-content/uploads/2018/06/Vikings-NETFLIX.jpg",
+    title: "A Netflix Original Series",
+    sname: "VIKINGS",
+    link: "https://www.netflix.com/in/title/70301870#:~:text=This%20gritty%20drama%20charts%20the,Creators%3AMichael%20Hirst",
+  },
+  {
+    imgsrc:
+      "https://gumlet.assettype.com/swarajya%2F2021-09%2F2948e20c-078b-40d2-91c7-42ae2aa7dcd2%2FNetflix_series__1_.png?format=auto",
+    title: "A Netflix Original Series",
+    sname: "KOTA FACTORY",
+    link: "https://www.netflix.com/in/title/81249783",
+  },
+];
+
+export default Sdata;
